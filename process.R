bf8808a83f355e5ecf9c20f87a18de6e6e1fa178
Jordan Ewings <ewin1507@stthomas.edu>
2019-05-20 10:36:51 -0500

    Documentation, removed unused HPI code and converted appropriate fields to numeric

--- process.R
+++ process.R
@@ -5,6 +5,8 @@ library(readxl)
 library(xlsx)
 library(datapasta)
 
+# List state crime files in WD and bind (union) to create one US data frame
+
 files <- list.files()
 tbl <- sapply(files, read_excel, simplify = FALSE) %>%
   bind_rows(.id = 'state')
@@ -13,9 +15,13 @@ AllStateCrimes <- tbl[, -14]
 
 colnames(AllStateCrimes) <- c('State', 'City', 'Population', 'Violent', 'Murder', 'Rape', 'Rape2', 'Robbery', 'Aggra', 'Property', 'Burglary', 'Larc', 'Motor')
 
+# Removing repeated titles within df (from the union) and trimming lengthy state column name
+
 AllStateCrimes <- AllStateCrimes %>%
   filter(!is.na(Population), Population != 'Population') %>%
   mutate(State = substr(State, 43, nchar(State) - 17))
+
+# Used data pasta to quickly create df of state abbreviations
 
 StateAbb <- data.frame(stringsAsFactors=FALSE,
        State = c("Alabama", "Alaska", "Arizona", "Arkansas", "California",
@@ -37,13 +43,16 @@ StateAbb <- data.frame(stringsAsFactors=FALSE,
                      "WI", "WY")
 )
 
+# Joining crime data with state abbreviations and removing column with entire State name
+# Key fields will be City and State abbreviation
+
 Crime <- StateAbb %>%
   merge.data.frame(AllStateCrimes, by = 'State') %>%
   select(-c('State', 'Rape2'))
 
 colnames(Crime) <- c('State', 'City', 'Population', 'Violent', 'Murder', 'Rape', 'Robbery', 'Aggravated Assault', 'Property', 'Burglary', 'Larceny', 'Motor Vehicle')
 
-####
+# Loading precipiation data and joining with crime df
 
 Precip <- read_excel("data/PrecipitationData.xlsx", 
                      col_names = c('State', 'City', 'AnnualPrecip'),
@@ -53,27 +62,11 @@ CrimeAndPrecip <- Crime %>%
   mutate(City = toupper(City)) %>%
   merge.data.frame(Precip, by = c('City', 'State'))
 
-####
+# Loading coordinates for mapping
 
 CityCoordinates <- read_csv("/data/CityCoordinates.csv", col_names = c('City', 'State', 'LAT', 'LNG'),   skip = 1)
 
-HPI_master2 <- HPI_master %>%
-  mutate(State = substr(State, 2, nchar(State))) %>%
-  select(c('City', 'State', 'index_nsa')) %>%
-  mutate(City = gsub("-.*","",City)) 
-
-HPI_master2$State <- HPI_master$State
-HPI_master <- HPI_master2 %>% 
-  mutate(State = substr(State, 1,2))
-
-AllData <- CityCoordinates %>%
-  mutate(City = toupper(City)) %>%
-  merge.data.frame(CrimeAndPrecip, by = c('City', 'State'))
-
-AllData2 <- AllData %>% 
-  merge.data.frame(HPI_master2, by = c('City', 'State'))
-
-#### Add House Price
+# Loading and joining house price data
 
 HousePrice <- read_csv("data/HousePrice.csv")
 
@@ -81,14 +74,15 @@ HousePrice <- HousePrice %>%
   mutate(HomeSalesPrice = parse_number(HousePrice$`Median Sale Price`)) %>%
   select(c('City', 'State', 'HomeSalesPrice'))
 
+# Finalizing data frame by merging house price data and converting appropriate fields to numeric
+
 AllData3 <-HousePrice %>%
   merge.data.frame(AllData, by = c('City', 'State'))
 
 AllData3 <- AllData3 %>%
   mutate(PropCrimeRatePer1000 = (parse_number(AllData3$Property)/parse_number(AllData3$Population)*1000))
 
-### Prep data for mapping
-
-AllData$LAT <- as.numeric(AllData$LAT)
-AllData$LNG <- as.numeric(AllData$LNG)
+AllData3[, c(3:17)] <- sapply(AllData3[, c(3:17)], as.numeric)
+AllData3 <- AllData3 %>%
+  mutate(PropCrimeRatePer1000 = round(PropCrimeRatePer1000, 2))
 
